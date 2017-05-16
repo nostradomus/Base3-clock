f191e45e099db3782f76f84891b728919dbab0db
Carl <GIT.nostradomus@gmail.com>
2017-05-16 16:28:36 +0200

    Add files via upload

---
+++ source/ternary_clock.ino
@@ -0,0 +1,46 @@
+//***************************************************
+//**              TERNARY CLOCK                    **
+//**                                               **
+//**   author : Carl - (c)2017                     **
+//**   version : O.9                               **
+//**   date : 01/05/2017                           **
+//**                                               **
+//**   https://github.com/nostradomus/Base3-clock  **
+//**                                               **
+//***************************************************
+
+//***************************************************
+//**             library section                   **
+//***************************************************
+#include <Wire.h>
+#include <EEPROM.h>                         // library to store the brightness level & alarm time in the onboard memory
+#include "RTClib.h"                         // real time clock library, see : https://github.com/adafruit/RTClib
+#include <Adafruit_NeoPixel.h>              // library required to control the WS2812B LED's, see : https://github.com/adafruit/Adafruit_NeoPixel
+
+//***************************************************
+//**            constants section                  **
+//***************************************************
+
+
+//***************************************************
+//**         global variables section              **
+//***************************************************
+
+
+//***************************************************
+//**               setup section                   **
+//***************************************************
+void setup() {
+
+  }
+
+//***************************************************
+//**            main application loop              **
+//***************************************************
+void loop() {
+
+  }
+
+//***************************************************
+//** application specific functions and procedures **
+//***************************************************
