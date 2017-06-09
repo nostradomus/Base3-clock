ac5c79fbaf1eea95751bfa772b8f70926ba9cbec
Carl <GIT.nostradomus@gmail.com>
2017-06-09 13:23:26 +0200

    Add files via upload

--- docs/project-mindmap.mm
+++ docs/project-mindmap.mm
@@ -4,34 +4,68 @@
 <icon BUILTIN="clock"/>
 <node CREATED="1493377595221" ID="ID_127670142" MODIFIED="1493378813893" POSITION="right" TEXT="Functionalities">
 <icon BUILTIN="list"/>
-<node CREATED="1493377672938" ID="ID_1008886243" MODIFIED="1493377735386" TEXT="show the time : 3x4 cells, OFF+2 colors"/>
-<node CREATED="1493377738480" ID="ID_249928181" MODIFIED="1493377887788" TEXT="keep time during power down"/>
-<node CREATED="1493377776854" ID="ID_1324779523" MODIFIED="1493377838485" TEXT="buttons to set the time"/>
-<node CREATED="1493377839033" ID="ID_114541236" MODIFIED="1493377862277" TEXT="switch light off at night"/>
+<node CREATED="1493377672938" ID="ID_1008886243" MODIFIED="1494603431323" TEXT="show the time : 3x4 cells, OFF+2 colors">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1493377738480" ID="ID_249928181" MODIFIED="1497005638488" TEXT="keep time during power down">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1493377776854" ID="ID_1324779523" MODIFIED="1494603441781" TEXT="buttons to set the time">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1493377839033" ID="ID_114541236" MODIFIED="1497005647150" TEXT="switch light off at night">
+<icon BUILTIN="button_ok"/>
+</node>
 </node>
 <node CREATED="1493377625799" ID="ID_1021585703" MODIFIED="1493378831010" POSITION="right" TEXT="Electronics">
 <icon BUILTIN="launch"/>
 <node CREATED="1493377909809" ID="ID_1383462043" MODIFIED="1493378244479" TEXT="time output">
-<node CREATED="1493377957849" ID="ID_475662683" MODIFIED="1493377985589" TEXT="option 1 : 22 I/O lines"/>
-<node CREATED="1493377986488" ID="ID_703909726" MODIFIED="1493378028530" TEXT="option 2 : I2C I/O expander"/>
-<node CREATED="1493378030002" ID="ID_1028523622" MODIFIED="1493378064329" TEXT="option 3 : 1-wire RGB LEDS"/>
-<node CREATED="1493378084758" ID="ID_938787730" MODIFIED="1493378185862" TEXT="extra 1 : MOSFET driver for high power LEDS"/>
-<node CREATED="1493378188010" ID="ID_1760774701" MODIFIED="1493378227129" TEXT="extra 2 : relay board for high voltage lights"/>
+<node CREATED="1493377957849" ID="ID_475662683" MODIFIED="1497005641902" TEXT="option 1 : 22 I/O lines">
+<icon BUILTIN="button_cancel"/>
 </node>
-<node CREATED="1493378246050" ID="ID_1512507721" MODIFIED="1493378278588" TEXT="I2C time chip with backup battery"/>
-<node CREATED="1493378279970" ID="ID_1745373207" MODIFIED="1493378298869" TEXT="two input buttons"/>
-<node CREATED="1493378300669" ID="ID_1500270845" MODIFIED="1493378319724" TEXT="one analogue input with LDR"/>
+<node CREATED="1493377986488" ID="ID_703909726" MODIFIED="1497005626794" TEXT="option 2 : I2C I/O expander">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1493378030002" ID="ID_1028523622" MODIFIED="1494603449353" TEXT="option 3 : 1-wire RGB LEDS">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1493378084758" ID="ID_938787730" MODIFIED="1497005723548" TEXT="extra 1 : MOSFET driver for high power LEDS -&gt; future project">
+<icon BUILTIN="xmag"/>
+</node>
+<node CREATED="1493378188010" ID="ID_1760774701" MODIFIED="1497005729615" TEXT="extra 2 : relay board for high voltage lights -&gt; future project">
+<icon BUILTIN="xmag"/>
+</node>
+</node>
+<node CREATED="1493378246050" ID="ID_1512507721" MODIFIED="1494603455462" TEXT="I2C time chip with backup battery">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1493378279970" ID="ID_1745373207" MODIFIED="1494603461423" TEXT="two input buttons">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1493378300669" ID="ID_1500270845" MODIFIED="1494603464878" TEXT="one analogue input with LDR">
+<icon BUILTIN="button_ok"/>
+</node>
 </node>
 <node CREATED="1493377644171" ID="ID_932688940" MODIFIED="1493378799531" POSITION="right" TEXT="Programming">
 <icon BUILTIN="penguin"/>
-<node CREATED="1493378328035" ID="ID_1559818651" MODIFIED="1493378372992" TEXT="function to translate time to the ternary numeral system"/>
-<node CREATED="1493378375618" ID="ID_1736341347" MODIFIED="1493378400212" TEXT="one second cyclic output update"/>
-<node CREATED="1493378402855" ID="ID_1112822866" MODIFIED="1493378439553" TEXT="interupt driven button input"/>
-<node CREATED="1493378440914" ID="ID_675253612" MODIFIED="1493378574937" TEXT="evaluate the ambient light level"/>
+<node CREATED="1493378328035" ID="ID_1559818651" MODIFIED="1494603471673" TEXT="function to translate time to the ternary numeral system">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1493378375618" ID="ID_1736341347" MODIFIED="1494603474879" TEXT="one second cyclic output update">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1493378402855" ID="ID_1112822866" MODIFIED="1497005681213" TEXT="interupt driven button input -&gt; no longer required">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1493378440914" ID="ID_675253612" MODIFIED="1497005389320" TEXT="evaluate the ambient light level">
+<icon BUILTIN="button_ok"/>
+</node>
 </node>
 <node CREATED="1493377657369" ID="ID_973610802" MODIFIED="1493378794192" POSITION="right" TEXT="Housing">
 <icon BUILTIN="gohome"/>
-<node CREATED="1493378590589" ID="ID_407702005" MODIFIED="1493378633718" TEXT="step 1 : small size prototype"/>
+<node CREATED="1493378590589" ID="ID_407702005" MODIFIED="1497005495506" TEXT="step 1 : small size prototype">
+<icon BUILTIN="button_ok"/>
+</node>
 <node CREATED="1493378613332" ID="ID_1372726148" MODIFIED="1493378648246" TEXT="step 2 : large size design"/>
 <node CREATED="1493378653428" ID="ID_336506699" MODIFIED="1493378670438" TEXT="step 3 : large size building drawings"/>
 </node>
